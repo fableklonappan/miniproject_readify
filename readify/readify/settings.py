621b0d61a20a583cea011f9c4f36f8bf6366b7c2
fableklonappan <fableklonappan20224@mca.ajce.in>
2023-08-16 13:55:19 +0530

    gh

--- readify/readify/settings.py
+++ readify/readify/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
